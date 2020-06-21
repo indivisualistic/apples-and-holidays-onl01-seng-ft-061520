9d60fda6418206d003ba44156312b96293efaa6d
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-21 17:49:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
      capitalized_holiday = holiday.to_s.split('_').each {|i| i.capitalize!}.join(' ')
      puts "  #{capitalized_holiday}: #{supplies.join(', ')}"
    end
-   
+ end
+ 
+
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
