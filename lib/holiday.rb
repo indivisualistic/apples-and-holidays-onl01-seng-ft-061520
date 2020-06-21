cd21ac5c94a449ab7594b6f4479bbe7f851e274b
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-21 18:09:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
   end
-  
+end
+
+
 
 
 
